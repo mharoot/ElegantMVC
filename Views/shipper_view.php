15ca6d22153cb16636c59a272cc71530344a2161
Michael Harootoonyan <michaelharootoonyan@yahoo.com>
2018-01-29 21:09:23 -0800

    fixed typo in class name

--- Views/shipper_view.php
+++ Views/shipper_view.php
@@ -1,40 +1,40 @@
 <?php
 
 /**
-* The Shippers page view
+* The Shipper page view
 */
-class ShippersView
+class ShipperView
 {
-    private $modelObj;
-    private $controller;
+  private $modelObj;
+  private $controller;
 
-    function __construct($controller, $model)
+  function __construct($controller, $model)
+  {
+      $this->controller = $controller;
+
+      $this->modelObj = $model;
+
+      print "Shippers - ";
+  }
+
+  /**
+   * Action Methods
+   */
+  public function shippers()
+  {
+    print "shippers";
+    $shippers = $this->controller->shippers();
+    echo '<ul>';
+    foreach(  $shippers as $shipper)
     {
-        $this->controller = $controller;
-
-        $this->modelObj = $model;
-
-        print "Shippers - ";
+      echo "<li>
+              <ul>
+                  <li>ShipperID: $shipper->ShipperID</li>
+                  <li>ShipperName: $shipper->ShipperName </li>  
+                  <li>EmployeeID: $shipper->EmployeeID </li>
+              </ul>
+            </li>";
     }
-
-    /**
-     * Action Methods
-     */
-    public function shippers()
-    {
-        print "shippers";
-        $shippers = $this->controller->shippers();
-        echo '<ul>';
-        foreach(  $shippers as $shipper)
-        {
-            echo "<li>
-                    <ul>
-                        <li>ShipperID: $shipper->ShipperID</li>
-                        <li>ShipperName: $shipper->ShipperName </li>  
-                        <li>EmployeeID: $shipper->EmployeeID </li>
-                    </ul>
-                    </li>";
-        }
-        echo '</ul>';
-    }
+    echo '</ul>';
+  }
 }
