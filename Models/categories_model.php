407c539006be8de8577d9afe6e134a5807dcc9ad
341megaman <christom525@yahoo.com>
2018-01-31 14:54:38 -0800

    Categories Model

---
+++ Models/categories_model.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+    class CategoriesModel extends Model 
+    {
+
+        //private $message = 'Welcome to Home page.';
+
+        function __construct()
+        {
+			$this->table_name = 'categories';
+			parent::__construct($this);
+        }
+
+        public function getCategories()
+        {
+            return $this->all();
+        }
+		
+		public function create($CategoryName , $Description)
+		{
+			$this->CategoryName = $CategoryName;
+			$this->Description = $Description;
+			$this->save();
+		}
+
+    }
+?>
