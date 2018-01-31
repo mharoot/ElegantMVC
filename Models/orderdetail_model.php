43d6722d9f1e1c77cd85e01728b73657de142591
cbuntrakulsuk <32884696+cbuntrakulsuk@users.noreply.github.com>
2018-01-31 15:04:25 -0800

    orderdetail_model with get function

---
+++ Models/orderdetail_model.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+class OrderDetailModel extends Model {
+
+    //public $id; id is being autoincremented no need to set.  we need auto incremeting detection ideas
+    // public $name;
+    // public $address;
+    /* 
+        Every instance of a model should have properties like table_name, primary key, etc;
+        Before the parent::__construct().
+    */
+    public function __construct()  
+    {  
+            // by convention Elegant assumes the model representing the table in the database is all lower case and plural
+            $this->table_name = 'orderdetails';
+            parent::__construct($this);
+    }
+
+       
+    public function getOrderDetails()
+    {
+        return $this->get();
+    }
+    
+}
+
+?>
