5ba5d9f242b36cc322544f443b4b8fbb51d00b76
341megaman <christom525@yahoo.com>
2018-02-18 15:44:07 -0800

    Allows review/edit of billing information

--- Controllers/customer_controller.php
+++ Controllers/customer_controller.php
@@ -17,4 +17,21 @@
         {
             return $this->modelObj->getCustomerOrders();
         }
+		
+		public function reviewBillingInformation()
+		{
+			return $this->modelObj->reviewBillingInformation();
+		}
+		
+		
+		public function editBillingInformation()
+		{
+			return $this->modelObj->reviewBillingInformation();
+		}
+		
+		public function insertNewBillingInformation($field, $value)
+		{
+			return $this->modelObj->updateById($field, $value);
+		}
+		
      }
