3597b2790e66d833cd7a8f5d1ecc61fa0b6c847a
mharoot <michaelharootoonyan@yahoo.com>
2018-03-06 18:54:45 -0800

    added SupplierModel::getSupplier() and SupplierModel::editBuisnessInformation()

--- Models/supplier_model.php
+++ Models/supplier_model.php
@@ -15,4 +15,34 @@ class SupplierModel extends Model
         return $this->get();
     }
 
+    /**
+     * Searches the suppliers table using foreign key = $_SESSION['user_id']
+     * @return SupplierModel with all columns of that user_id or FALSE
+     */
+    private function getSupplier()
+    {
+        return $this->where('UserID', '=', $_SESSION['user_id'])
+             ->single();
+    }
+
+    /**
+     * Searches the suppliers table using foreign key = $_SESSION['user_id']
+     * 
+     * @return true if sucessfully inserted or updated Buisness Information
+     */
+    public function editBuisnessInformation()
+    {
+        $supplier = $this->getSupplier();
+        if (isset($supplier))
+        {
+            // get ready to update
+            
+        }
+        else
+        {
+            // get ready to insert
+
+        }
+    }
+
 }
