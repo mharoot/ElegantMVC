d69f3001149e23b3ae3c43cc043a455412c76e2a
mharoot <michaelharootoonyan@yahoo.com>
2018-03-31 19:05:50 -0700

    added test_Adding_Additional_Properties_To_Data_Collection()

--- Tests/OrderTest.php
+++ Tests/OrderTest.php
@@ -6,19 +6,60 @@ use PHPUnit\Framework\TestCase;
 
 include_once('Elegant/Model.php');
 include_once('Models/order_model.php');
+include_once('Models/product_model.php');
+include_once('Models/supplier_model.php');
 // include_once('Models/user_model.php');
 class OrderTest extends TestCase
 {
 
-    public function test_one_to_many_order_and_order_details_get() 
-    {
-        $model = new OrderModel();
-        $orderDetails = $model->oneToMany('orderdetails', 'OrderID', 'OrderID')
-                              ->where('orderdetails.OrderID', '=', 10248)
-                              ->get(['ProductID']);
-        
-        var_dump($orderDetails[0]);
-        $this->assertTrue(  $orderDetails != null  );
+  public function test_one_to_many_order_and_order_details_get() 
+  {
+      $order_model = new OrderModel();
+      $orderDetails = $order_model->oneToMany('orderdetails', 'OrderID', 'OrderID')
+                                  ->where('orderdetails.OrderID', '=', 10248)
+                                  ->get();
 
-    }
+      $product_model = new ProductModel();
+      foreach($orderDetails as $od) {
+          $productName =  $product_model->where('ProductID', '=', $od->ProductID)
+                                        ->single(['ProductName']);
+      }
+      $this->assertTrue(  $orderDetails != null  );
+
+  }
+
+
+  public function test_Adding_Additional_Properties_To_Data_Collection() 
+  {
+      $arr = [];
+      $order_model = new OrderModel();
+      $orderDetails = $order_model->oneToMany('orderdetails', 'OrderID', 'OrderID')
+                                  ->where('orderdetails.OrderID', '=', 10248)
+                                  ->get();
+
+      // Adding Additional Properties To $orderDetails data collection
+      $product_model = new ProductModel();
+      $supplier_model = new SupplierModel();
+      foreach($orderDetails as $od) {
+        $pd = $product_model->oneToOne('categories', 'CategoryID', 'CategoryID')
+                            ->where('ProductID', '=', $od->ProductID)
+                            ->single(['CategoryName', 'ProductName', 'SupplierID', 'Unit', 'Price']);
+   
+        $od->CategoryName = $pd->CategoryName;
+        $od->ProductName = $pd->ProductName;
+        $od->SupplierName = $supplier_model->where('SupplierID', '=', $pd->SupplierID)->single()->SupplierName;
+      }
+
+      foreach($orderDetails as $od) {
+        $this->assertTrue(  $od->CategoryName != null  );
+        $this->assertTrue(  $od->ProductName  != null  );
+        $this->assertTrue(  $od->SupplierName != null  );
+      }
+
+      var_dump($orderDetails);
+
+  }
+
+
+
 }
