6429500bbe450b081e3090c0186fd31423adb7c6
mharoot <michaelharootoonyan@yahoo.com>
2018-04-25 18:35:37 -0700

    finished Employee::getCustomerOrders()

--- Models/employee_model.php
+++ Models/employee_model.php
@@ -12,17 +12,6 @@ class EmployeeModel extends Model
 
     /**
      * Searches the employees table using foreign key = $_SESSION['user_id']
-     * @return EmployeeModel with all columns of that user_id or FALSE
-     */
-    public function getEmployee()
-    {
-        return $this->where('UserID', '=', $_SESSION['user_id'])
-                    ->single();
-    }
-
-
-    /**
-     * Searches the employees table using foreign key = $_SESSION['user_id']
      * Update if employee exists or insert if employee does not exist
      * @return true if successfully inserted or updated Employee Information
      */
@@ -55,28 +44,42 @@ class EmployeeModel extends Model
         return $employee_info_edited;
     }
 
+    /**
+     * As an Employee I want to be able to view customer orders that I had the responsibility of full-filling.
+     * Im interested in the status for javascript filter, picking a shipping company, and thats it. 
+     */
+    public function getCustomerOrders()
+    {
+        $employee = $this->getEmployee(); // get logged in employee
+        return $this->oneToMany('orders', 'EmployeeID', 'EmployeeID')
+                    ->where('employees.EmployeeID', '=', $employee->EmployeeID)
+                    ->orWhere('orders.OrderStatus', '=',  0)
+                    ->get(["orders.*"]);
+            
+             
+        
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    /**
+     * Searches the employees table using foreign key = $_SESSION['user_id']
+     * @return EmployeeModel with all columns of that user_id or FALSE
+     */
+    public function getEmployee()
+    {
+        return $this->where('UserID', '=', $_SESSION['user_id'])
+                    ->single();
+    }
 
     public function getEmployees()
     {
         return $this->get();
+    }
+
+    public function getSuppliers()
+    {
+        $SupplierModel = new SupplierModel();
+        return $SupplierModel->get();
     }
 
 
